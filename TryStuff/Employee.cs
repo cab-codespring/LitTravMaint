bfbf4d98570fc146e267cefca00298814a5cdba7
Christina Burnham <Christina.Burnham@gmail.com>
2012-01-25 17:49:37 -0500

    Chris's experimental UI stuff in TryStuff project

---
+++ TryStuff/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TryStuff
+{
+    public class Employee
+    {
+        public Employee(string fname, string lname, int id)
+        {
+            FirstName = fname;
+            LastName = lname;
+            EmployeeID = id;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EmployeeID { get; set; }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ TryStuff/TryStuffMainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace TryStuff
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class TryStuffMainWindow : Window
+    {
+        ObservableCollection<Employee> _listOfEmployees = new ObservableCollection<Employee>();
+
+        public ObservableCollection<Employee> ListOfEmployees
+        {
+            get { return _listOfEmployees; }
+            set { _listOfEmployees = value; }
+        }
+
+        public TryStuffMainWindow()
+        {
+            InitializeComponent();
+
+            //this.DataContext = this; // this is important
+
+
+            InitListOfEmployees();
+
+        }
+
+        void InitListOfEmployees()
+        {
+            ListOfEmployees.Add(new Employee("Chris","Bennet",200));
+            ListOfEmployees.Add(new Employee("Chrissy","Burnham",100));
+            ListOfEmployees.Add(new Employee("GoldenBoy","Burnham",300));
+
+        }
+    }
+}
