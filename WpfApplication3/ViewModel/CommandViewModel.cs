adb81c88f2a2004633981c3b6dd3c06afe600455
Christina Burnham <Christina.Burnham@gmail.com>
2012-01-31 12:29:31 -0500

    added display name

--- WpfApplication3/ViewModel/CommandViewModel.cs
+++ WpfApplication3/ViewModel/CommandViewModel.cs
@@ -10,12 +10,18 @@ namespace LitTravProj.ViewModel
     public class CommandViewModel : ReactiveObject
 
     {
+        /// <summary>
+        /// Returns the user-friendly name of this object.
+        /// Child classes can set this property to a new value,
+        /// or override it to determine the value on-demand.
+        /// </summary>
+        public virtual string DisplayName { get; protected set; }
+
         public CommandViewModel(string displayName, ICommand command)
         {
             if (command == null)
                 throw new ArgumentNullException("command");
-
-          //  base.DisplayName = displayName;
+            this.DisplayName = displayName;
             this.Command = command;
         }
 
