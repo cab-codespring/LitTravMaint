8a73926c8eb6a9cd3aa888802b5d4632bdf40451
Christina Burnham <Christina.Burnham@gmail.com>
2012-04-14 05:51:27 -0400

    the begenning of a report such as it is

---
+++ WpfApplication3/Reports/OrderInvoiceForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace LitTravProj.Reports
+{
+    partial class OrderInvoiceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer2 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.OrderInvoiceViewBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.OrderInvoiceViewBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer2
+            // 
+            this.reportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.OrderInvoiceViewBindingSource;
+            this.reportViewer2.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer2.LocalReport.ReportEmbeddedResource = "LitTravProj.Reports.Report2.rdlc";
+            this.reportViewer2.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer2.Name = "reportViewer2";
+            this.reportViewer2.ServerReport.ReportServerUrl = new System.Uri("", System.UriKind.Relative);
+            this.reportViewer2.Size = new System.Drawing.Size(1003, 471);
+            this.reportViewer2.TabIndex = 1;
+            // 
+            // OrderInvoiceViewBindingSource
+            // 
+            this.OrderInvoiceViewBindingSource.DataSource = typeof(LitTravData.Model.OrderInvoiceView);
+            // 
+            // OrderInvoiceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 471);
+            this.Controls.Add(this.reportViewer2);
+            this.Name = "OrderInvoiceForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.OrderInvoiceForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OrderInvoiceViewBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer2;
+        private System.Windows.Forms.BindingSource OrderInvoiceViewBindingSource;
+    }
+}
